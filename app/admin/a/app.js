db1ae295f9731876e4d5506d415ec06caa983381
Pankaj Kumar <pankajche1@gmail.com>
2016-08-14 20:29:18 +0530

    some pages for admin A section developed for testing menu bar close after link click

--- app/admin/a/app.js
+++ app/admin/a/app.js
@@ -10,6 +10,15 @@ require('./directives');
 app.config(['$routeProvider',function($routeProvider){
 		$routeProvider.when('/welcome',{
 				templateUrl:'/admin/a/welcome.html',controller:'WelcomeController'});
+		$routeProvider.when('/products',{
+				templateUrl:'/admin/a/products.html',
+                                controller:'ProductsController'});
+		$routeProvider.when('/emplopyees',{
+				templateUrl:'/admin/a/employees.html',
+                                controller:'EmployeesController'});
+		$routeProvider.when('/settings',{
+				templateUrl:'/admin/a/settings.html',
+                                controller:'SettingsController'});
 		$routeProvider.otherwise({redirectTo:'/welcome'});
 }]);//config 
 //change here for watch
--- app/admin/a/controllers/index.js
+++ app/admin/a/controllers/index.js
@@ -3,6 +3,9 @@
 //console.log('here');
 var app = angular.module('app');
 app.controller('WelcomeController', require('./welcome-controller'));//controller
+app.controller('ProductsController', require('./products-controller'));//controller
+app.controller('EmployeesController', require('./employees-controller'));//controllerm
+app.controller('SettingsController', require('./settings-controller'));//controller
 
 
 
